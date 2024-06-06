3cd123afeef0665b31c591cf929a45ca88c80064
PNKim <paljapon.nait@gmail.com>
2024-06-06 13:58:34 +0700

    feat: able to like or dislike post

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,30 +1,72 @@
+import { useState } from "react";
+import PostObject from "./postObject";
+
 function Posts() {
+  const [like, setLike] = useState(0);
+  const [indexLike, setIndexLike] = useState({});
+  const addLike = (like, index) => {
+    const add = { ...indexLike };
+    if (!add[index]) {
+      add[index] = like;
+    }
+    add[index]++;
+    setIndexLike(add);
+    setLike(add);
+  };
+  const minusLike = (like, index) => {
+    const minus = { ...indexLike };
+    if (!minus[index]) {
+      minus[index] = like;
+    }
+    minus[index]--;
+    if (minus[index] <= 0) {
+      minus[index] = "0";
+    }
+    setIndexLike(minus);
+    setLike(minus);
+  };
   return (
-    <div class="app-wrapper">
-      <h1 class="app-title">Posts</h1>
-      <div class="post-list">
-        <div class="post-item">
-          <div class="post-header">
-            <h2>Post Title #1</h2>
-            <div class="post-social-media-stats">
-              <span class="stats-topic">Likes: </span>
-              <span class="post-likes">10</span>
+    <>
+      <div class="app-wrapper">
+        <h1 class="app-title">Posts</h1>
+        {PostObject.map((post, index) => {
+          return (
+            <div key={index} class="post-list">
+              <div class="post-item">
+                <div class="post-header">
+                  <h2>{post.title}</h2>
+                  <div class="post-social-media-stats">
+                    <span class="stats-topic">Likes: </span>
+                    <span class="post-likes">
+                      {like[index] ? like[index] : post.likes}
+                    </span>
+                  </div>
+                </div>
+                <p class="post-content">{post.content}</p>
+                <div class="post-actions">
+                  <button
+                    class="like-button"
+                    onClick={() => {
+                      addLike(post.likes, index);
+                    }}
+                  >
+                    Like
+                  </button>
+                  <button
+                    class="dislike-button"
+                    onClick={() => {
+                      minusLike(post.likes, index);
+                    }}
+                  >
+                    Dislike
+                  </button>
+                </div>
+              </div>
             </div>
-          </div>
-          <p class="post-content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-            vel turpis vestibulum, aliquet ipsum vitae, auctor risus. Morbi
-            tincidunt, leo non molestie consectetur, elit libero faucibus
-            tellus, sed fringilla tortor libero sit amet odio. Maecenas sed ante
-            condimentum mauris euismod pellentesque eu eu justo...
-          </p>
-          <div class="post-actions">
-            <button class="like-button">Like</button>
-            <button class="dislike-button">Dislike</button>
-          </div>
-        </div>
+          );
+        })}
       </div>
-    </div>
+    </>
   );
 }
 

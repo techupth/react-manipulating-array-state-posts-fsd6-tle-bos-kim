b3185c3e3de55430b00ad66d7d3b41acd40435e5
PNKim <paljapon.nait@gmail.com>
2024-06-06 17:31:25 +0700

    fix:

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -24,47 +24,45 @@ function Posts() {
     setLike(minus);
   };
   return (
-    <>
-      <div class="app-wrapper">
-        <h1 class="app-title">Posts</h1>
+    <div class="app-wrapper">
+      <h1 class="app-title">Posts</h1>
+      <div class="post-list">
         {PostObject.map((post, index) => {
           return (
-            <div key={index} class="post-list">
-              <div class="post-item">
-                <div class="post-header">
-                  <h2>{post.title}</h2>
-                  <div class="post-social-media-stats">
-                    <span class="stats-topic">Likes: </span>
-                    <span class="post-likes">
-                      {like[index] ? like[index] : post.likes}
-                    </span>
-                  </div>
+            <div key={index} class="post-item">
+              <div class="post-header">
+                <h2>{post.title}</h2>
+                <div class="post-social-media-stats">
+                  <span class="stats-topic">Likes: </span>
+                  <span class="post-likes">
+                    {like[index] ? like[index] : post.likes}
+                  </span>
                 </div>
-                <p class="post-content">{post.content}</p>
-                <div class="post-actions">
-                  <button
-                    class="like-button"
-                    onClick={() => {
-                      addLike(post.likes, index);
-                    }}
-                  >
-                    Like
-                  </button>
-                  <button
-                    class="dislike-button"
-                    onClick={() => {
-                      minusLike(post.likes, index);
-                    }}
-                  >
-                    Dislike
-                  </button>
-                </div>
+              </div>
+              <p class="post-content">{post.content}</p>
+              <div class="post-actions">
+                <button
+                  class="like-button"
+                  onClick={() => {
+                    addLike(post.likes, index);
+                  }}
+                >
+                  Like
+                </button>
+                <button
+                  class="dislike-button"
+                  onClick={() => {
+                    minusLike(post.likes, index);
+                  }}
+                >
+                  Dislike
+                </button>
               </div>
             </div>
           );
         })}
       </div>
-    </>
+    </div>
   );
 }
 
